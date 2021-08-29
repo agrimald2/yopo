e70235865f048297a22e9999f19e596fab24242d
Fercho Posting <shaaf4ever@gmail.com>
2021-08-29 14:21:01 -0500

    Fillable

--- app/ProductQuestion.php
+++ app/ProductQuestion.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductQuestion extends Model
 {
+    protected $fillable = ['product_id', 'question'];
+
     public function product()
     {
         return $this->belongsTo(Product::class);
