7d19ceadb995548da3a114901bbd86e738726ac3
Fercho Posting <shaaf4ever@gmail.com>
2021-09-20 19:15:46 -0500

    Product list

--- resources/js/views/product/Components/ProductAditionals.vue
+++ resources/js/views/product/Components/ProductAditionals.vue
@@ -1,8 +1,26 @@
 <template>
   <div>
-    <div class="text-2xl">Adicionales del Producto</div>
+    <div class="text-2xl">Añadir un producto adicional</div>
     <div class="mt-6"></div>
     <Autocomplete @picked="onAditionalPicked" />
+
+    <!-- Aditionals list -->
+
+    <div class="mt-6 text-2xl">Productos Adicionales Actuales</div>
+    <div class="mt-3 grid gap-4">
+      <div
+        class="border rounded flex items-center p-2 gap-4"
+        v-for="x in 4"
+      >
+        <div class="font-bold text-sm">S/. 40</div>
+        <div class="flex-grow">Pollo Frito</div>
+        <div
+          class="underline text-red-500 cursor-pointer"
+          @click="onDelete"
+        >Quitar</div>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -17,6 +35,10 @@ export default {
   methods: {
     onAditionalPicked(id) {
       alert(id);
+    },
+
+    onDelete() {
+      alert("delete");
     }
   }
 };
